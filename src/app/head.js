6c31bf75adb55bc1b02d7e623d1b90c305322985
Robert Vázquez <52977024+RobertCoderDev@users.noreply.github.com>
2024-07-05 21:00:07 -0600

    se agrego el link canonical

--- src/app/head.js
+++ src/app/head.js
@@ -1,6 +1,7 @@
 export default function Head() {
     return (
         <>
+            <link rel="canonical" href="https://www.robertcoder.com/" />
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
             <link href="https://fonts.googleapis.com/css2?family=Patrick+Hand&display=swap" rel="stylesheet" />
